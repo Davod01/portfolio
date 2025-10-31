bc843323e43a66f0353587c28b638e94daab58aa
Davod01 <davod.nar@gmail.com>
2025-11-01 01:57:11 +0330

    first commit

---
+++ _nuxt/B0vrdL-v.js
@@ -0,0 +1,1 @@
+import{z as s}from"./B0rPK7oY.js";const o=s("/img/20251029_0410_3D Avatar.png");export{o as _};
---
+++ _nuxt/DDdd7iHp.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as t,H as c}from"./B0rPK7oY.js";const n={};function s(e,a){return t(),r("div",null,[c(e.$slots,"default")])}const _=o(n,[["render",s]]);export{_ as default};
