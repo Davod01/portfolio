f569c737836d84277063e3d33892d4198e30fc33
Davod01 <davod.nar@gmail.com>
2025-11-01 04:12:56 +0330

    version 0.03

---
+++ _nuxt/Ca-KWgZs.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as t,H as c}from"./B3zea147.js";const n={};function s(e,a){return t(),r("div",null,[c(e.$slots,"default")])}const _=o(n,[["render",s]]);export{_ as default};
---
+++ _nuxt/REtSuDUs.js
@@ -0,0 +1,1 @@
+import{z as s}from"./B3zea147.js";const o=s("/img/20251029_0410_3D Avatar.png");export{o as _};
